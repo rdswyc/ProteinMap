7461cfea805fbe3c762fa2d4f451b5bc0f7d6297
Rodrigo Chin <rodrigo.chin@outlook.com>
2023-07-14 18:57:22 -0300

    Implement models and add migration

--- midterm/proteinmap/models.py
+++ midterm/proteinmap/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Organism(models.Model):
+    taxa_id = models.IntegerField(primary_key=True, blank=False)
+    clade = models.CharField(max_length=2, null=False, blank=False)
+    genus = models.CharField(max_length=50, null=False, blank=False)
+    species = models.CharField(max_length=100, null=False, blank=False)
+
+    def get_scientific_name(self):
+        return self.genus + ' ' + self.species
+
+class Protein(models.Model):
+    protein_id = models.CharField(primary_key=True, max_length=12, blank=False)
+    length = models.IntegerField(null=False, blank=False)
+    organism = models.ForeignKey(Organism, null=False, on_delete=models.CASCADE)
+
+class Sequence(models.Model):
+    protein = models.OneToOneField(Protein, on_delete=models.CASCADE, primary_key=True)
+    sequence = models.CharField(max_length=40000, null=False, blank=True)
+
+class Pfam(models.Model):
+    pfam_id = models.CharField(primary_key=True, max_length=20, blank=False)
+    description = models.CharField(max_length=100, null=False, blank=False)
+
+class Domain(models.Model):
+    protein = models.ForeignKey(Protein, on_delete=models.CASCADE)
+    pfam = models.ForeignKey(Pfam, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    start = models.IntegerField(null=False, blank=False)
+    stop = models.IntegerField(null=False, blank=False)
