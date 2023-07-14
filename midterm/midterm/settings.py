1de60637e2177ef3de1508d2fdb9ddafc8f4726d
Rodrigo Chin <rodrigo.chin@outlook.com>
2023-07-14 15:20:19 -0300

    Add protein app files

--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'proteinmap.apps.ProteinmapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
