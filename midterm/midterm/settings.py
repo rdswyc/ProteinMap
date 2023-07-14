c4935f5be2272563181ca14aa446b36aecc1cc20
Rodrigo Chin <rodrigo.chin@outlook.com>
2023-07-14 20:47:24 -0300

    Install django rest framework and add api urls

--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'proteinmap.apps.ProteinmapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
