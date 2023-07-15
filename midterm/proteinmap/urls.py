5d0c3ab15824b1917ce6f01c91b916e335f4170d
Rodrigo Chin <rodrigo.chin@outlook.com>
2023-07-15 19:16:39 -0300

    Add api schema and swagger endpoint

--- midterm/proteinmap/urls.py
+++ midterm/proteinmap/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import RedirectView, TemplateView
+from rest_framework.schemas import get_schema_view
+
 
 from . import api
 
 urlpatterns = [
+    re_path(r'^$', RedirectView.as_view(
+        url='swagger-ui/',
+        permanent=False
+    ), name='index'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+    path('openapi', get_schema_view(
+        title="Protein map",
+        description="By Rodrigo Chin",
+        version="0.1.0"
+    ), name='openapi-schema'),
     path('api/protein/', api.ProteinCreate.as_view()),
     path('api/protein/<str:pk>/', api.ProteinDetail.as_view()),
     path('api/pfam/<str:pk>/', api.PfamDetail.as_view()),
